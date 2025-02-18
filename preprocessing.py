94354602af85336309b35ecd97c8a23700d070d6
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-02-18 23:11:12 +0100

    filter data by voronoi regions and area

--- preprocessing.py
+++ preprocessing.py
@@ -2,45 +2,16 @@ import argparse
 import prepare_data
 import pandas as pd 
 import numpy as np
-from scipy.spatial import Voronoi,  voronoi_plot_2d
-
-class VoronoiPreprocess:
-    def __init__(self, df):
-        print("Voronoi preprocessor initialized")
-        self.df=df
-        self.df = self.df.dropna(subset=['Center x coordinate', 'Center y coordinate']).drop_duplicates()
-        self.points = np.column_stack((df["Center x coordinate"], df["Center y coordinate"]))
-        self.voronoi = Voronoi(self.points)
-        self.regions= self.voronoi.regions
-        self.point_to_region = self.voronoi.point_region
-        print("Number of points: ", len(self.points))
-        print("Number of regions: ", len(self.regions))
-        print("Number of connections :", len(self.point_to_region))
-
-
-    def mark_points_without_regions(self):
-        region_list = np.array([])
-        valid_points = np.array([])
-        for i in range(len(self.point_to_region)):
-            if self.point_to_region[i] in region_list:
-                valid_points = np.append(valid_points, 0)
-            else:
-                valid_points = np.append(valid_points, 1)
-                region_list = np.append(region_list, self.point_to_region[i])
-
-        new_column = np.reshape(valid_points, (len(self.point_to_region), 1))
-        updated_data = np.hstack((self.df, new_column))
-        np.savetxt("new_points.dat", updated_data, fmt="%.3f")
-        return len(region_list)
-
 
 
 def main(args):
-    df = prepare_data.read_original_data(args.data_path)
-    voronoi_preprocessor = VoronoiPreprocess(df)
-    print("Points that have a region: ", voronoi_preprocessor.mark_points_without_regions())
-
-
+    df = prepare_data.read_data(args.data_path)
+    voronoi_preprocessor = prepare_data.VoronoiPreprocess(df)
+    areas=voronoi_preprocessor.calculate_areas()
+    updated_data = voronoi_preprocessor.mark_points_without_regions()
+    updated_data = voronoi_preprocessor.mark_points_with_big_area(areas, args.area_limit)
+    print(updated_data)
+    np.savetxt("new_points.dat", updated_data, fmt="%.3f")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("parser for solution pipes pattern analysis")
