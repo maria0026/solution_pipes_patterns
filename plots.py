af02b2b19b60d02eb332b00f0cfeff1b3c2aaeb6
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-02-18 23:12:38 +0100

    voronoi diagram with filtered points

--- plots.py
+++ plots.py
@@ -28,25 +28,29 @@ class Voronoi_Plotter(VoronoiAnalyser):
         #ax.set_ylim(5752275, 5752325)
         plt.show()
 
-    def all_voronoi_diagram_area_filtered(self):
+    def all_voronoi_diagram_area_filtered(self, area_limit):
         areas=self.calculate_areas()
         fig, ax = plt.subplots(figsize=(8, 6))
         voronoi_plot_2d(self.voronoi, ax=ax, show_vertices=False, line_width=2, line_colors='blue')
-        ax.scatter(self.points[:, 0], self.points[:, 1], s=1, color='red', zorder=5, label="Points")
+        for i, point in enumerate(self.points):
+            color = 'blue' if self.good_point.iloc[i] else 'red'  # Assuming True/False values in 'Point of Voronoi'
+            ax.scatter(point[0], point[1], s=20, color=color, zorder=5)
+
         ax.set_title("Voronoi Diagram", fontsize=16)
         ax.set_xlabel("X Coordinate", fontsize=14)
         ax.set_ylabel("Y Coordinate", fontsize=14)
         ax.grid(True)
         ax.legend()
-        ax.set_xlim(531925, 531975)
-        ax.set_ylim(5752275, 5752325)
+        #ax.set_xlim(531925, 531975)
+        #ax.set_ylim(5752275, 5752325)
 
         for j, area in enumerate(areas):
-            if not area>14:
+            if not area>area_limit:
                 polygon = [self.vertices[i] for i in self.regions[j]]
                 plt.fill(*zip(*polygon))
 
         plt.show()
+
 
     def areas_hist(self,areas):
         plt.hist(areas, bins=20, range=(0,13), edgecolor='black', label=f'Mean {np.mean(areas):.2f}, std {np.std(areas):.2f}')
