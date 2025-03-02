58b003a86f3bc48271e5aad0333734ff76c1a659
Magda__K <me.kurek@student.uw.edu.pl>
2025-03-03 00:49:40 +0100

    Modified code to work on all data files

--- prepare_data.py
+++ prepare_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from voronoi import BaseVoronoi
 
-def read_data(path, preprocessed=False):
+def read_data(path, preprocessed=False, add_geometric_center = False):
     """
     Reads data from a file and converts it to a pandas DataFrame.
 
@@ -37,11 +37,11 @@ def read_data(path, preprocessed=False):
 
 
 
-    if not preprocessed:
-        x_center_mass= df["Center x coordinate"].mean()
-        y_center_mass= df["Center y coordinate"].mean()
-        df["Center x coordinate"] = df["Center x coordinate"] - x_center_mass
-        df["Center y coordinate"] = df["Center y coordinate"] - y_center_mass
+    if add_geometric_center:
+        x_center= df["Center x coordinate"].mean()
+        y_center= df["Center y coordinate"].mean()
+        df["Center x coordinate"] = df["Center x coordinate"] - x_center
+        df["Center y coordinate"] = df["Center y coordinate"] - y_center
 
 
     if preprocessed:
@@ -58,10 +58,10 @@ def prepare_mock_data(x_min, x_max, y_min, y_max, num_poins):
     x_random_points = np.random.uniform(x_min, x_max, size = num_poins).reshape(-1, 1)
     y_random_points = np.random.uniform(y_min, y_max, size = num_poins).reshape(-1, 1)
     
-    x_center_mass  = np.mean(x_random_points)
-    y_center_mass  = np.mean(y_random_points)
-    x_random_points = x_random_points - x_center_mass
-    y_random_points = y_random_points - y_center_mass
+    x_center = np.mean(x_random_points)
+    y_center = np.mean(y_random_points)
+    x_random_points = x_random_points - x_center
+    y_random_points = y_random_points - y_center
 
     points = np.hstack((pipe_radius, x_random_points, y_random_points))
     random_df = pd.DataFrame(points, columns=["Pipe radius", "Center x coordinate", "Center y coordinate"])
--- preprocessing.py
+++ preprocessing.py
@@ -6,32 +6,43 @@ import numpy as np
 
 def main(args):
     #preprocessing of our data
-    df = prepare_data.read_data(args.data_path, preprocessed=False)
+    if args.data_path == "pipes3.dat":
+        df = prepare_data.read_data(args.data_path, preprocessed=False, add_geometric_center = True)
+    else:
+        df = prepare_data.read_data(args.data_path, preprocessed=False, add_geometric_center = False)
+
     voronoi_preprocessor = prepare_data.VoronoiPreprocess(df)
     areas=voronoi_preprocessor.calculate_areas()
     updated_data = voronoi_preprocessor.mark_points_without_regions()
     updated_data = voronoi_preprocessor.mark_points_with_big_area(areas, args.area_limit)
     print(updated_data)
-    np.savetxt("new_points.dat", updated_data, fmt="%.3f")
+    
+    if args.data_path.startswith("_"): 
+        np.savetxt("new" + args.data_path, updated_data, fmt="%.3f")
+    else:
+        np.savetxt("new_" + args.data_path, updated_data, fmt="%.3f")
 
 
     #generating and preprocessing random data
-    random_df=prepare_data.prepare_mock_data(args.x_min, args.x_max, args.y_min, args.y_max, args.num_points)
+    x_min, x_max = df["Center x coordinate"].min(), df["Center x coordinate"].max()
+    y_min, y_max = df["Center y coordinate"].min(), df["Center y coordinate"].max()
+    num_points = len(voronoi_preprocessor.points)
+    random_df=prepare_data.prepare_mock_data(x_min, x_max, y_min, y_max, num_points)
     voronoi_preprocessor = prepare_data.VoronoiPreprocess(random_df)
     areas=voronoi_preprocessor.calculate_areas()
     updated_data = voronoi_preprocessor.mark_points_without_regions()
     updated_data = voronoi_preprocessor.mark_points_with_big_area(areas, area_limit = args.area_limit)
-    np.savetxt("new_points_random.dat", updated_data, fmt="%.3f")
+
+    if args.data_path.startswith("_"):
+        np.savetxt("random" + args.data_path, updated_data, fmt="%.3f")
+    else:
+        np.savetxt("random_" + args.data_path, updated_data, fmt="%.3f")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("parser for solution pipes pattern analysis")
-    parser.add_argument("--data_path", nargs="?", default="pipes3.dat", help="path of data", type=str)
+    parser.add_argument("--data_path", nargs="?", default= "_SICILY.dat", help="path of data", type=str)
     parser.add_argument("--area_limit", nargs="?", default=14.0, help="limit of area", type=float)
-    parser.add_argument("--x_min", nargs="?", default=531840.694, help="Minimum X coordinate", type=float)
-    parser.add_argument("--x_max", nargs="?", default=532015.134, help="Maximum X coordinate", type=float)
-    parser.add_argument("--y_min", nargs="?", default=5752195.773, help="Minimum Y coordinate", type=float)
-    parser.add_argument("--y_max", nargs="?", default=5752549.147, help="Maximum Y coordinate", type=float)
-    parser.add_argument("--num_points", nargs="?", default=5356, help="Number of points", type=int)
 
     args = parser.parse_args()
     main(args)
