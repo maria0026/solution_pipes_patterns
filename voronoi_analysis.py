ebc48ee51dfc1b1b2808b051191e020bb8e14fc6
Mylord3000 <130466625+Mylord3000@users.noreply.github.com>
2025-03-25 17:44:51 +0100

    Update voronoi_analysis.py

--- voronoi_analysis.py
+++ voronoi_analysis.py
@@ -4,6 +4,9 @@ from scipy.spatial import Voronoi,  voronoi_plot_2d
 from voronoi import BaseVoronoi
 import pandas as pd
 from scipy.spatial import distance_matrix
+import math
+from typing import Tuple
+from shapely.geometry import Point, Polygon
 
 class VoronoiAnalyser(BaseVoronoi):
     def __init__(self, df):
@@ -61,7 +64,6 @@ class VoronoiAnalyser(BaseVoronoi):
                     sum_theta += np.exp(1j * 6 * theta_jk)  # Apply 6-fold symmetry
                 
                 psi[j] = sum_theta / N_j 
-                print(psi[j])
                 if absolute:
                     self.df['Hexatic order']=np.abs(psi)
                 else:
@@ -103,4 +105,94 @@ class VoronoiAnalyser(BaseVoronoi):
 
         return ripley_df
 
+    def find_points(self, x_center, y_center, radius_min, radius_max):
+        N=0
+        for point in self.points:
+            if point[0]!=x_center and point[1]!=y_center:
+                distance = math.sqrt((point[0] - x_center) ** 2 + (point[1] - y_center) ** 2)
+                if distance <= radius_max and distance>=radius_min:
+                    N += 1
+        return N
 
+    def calculate_mean_density(self):
+        #R = int(np.min(np.ptp(self.points, axis=0))/(2))
+        '''
+        x_bounds = (np.abs(self.points[0, :].max())-np.abs(self.points[0, :].min()))/2
+        y_bounds = (np.abs(self.points[1, :].max())-np.abs(self.points[1, :].min()))/2
+        R=np.sqrt(x_bounds**2+y_bounds**2)
+        max_area = np.pi*R**2
+        N=self.find_points(0, 0, 0, R)
+        print(N)
+        '''
+        N=len(self.points)
+        hull=self.convex_hull_creation()
+        max_area=self.hull_area(hull)
+        n=N/max_area
+        return n
+        
+    def intersection_area(self, circle_center, circle_radius, polygon_points):
+        circle = Point(circle_center).buffer(circle_radius)
+        polygon = Polygon(polygon_points)
+        intersection = circle.intersection(polygon)
+        return intersection.area
+    
+    def convex_hull_creation(self):
+        points_sorted = sorted(self.points, key=lambda x: x[0])
+        def cross(o, a, b):
+            return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+            
+        lower = []
+        for p in points_sorted:
+            while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+                lower.pop()
+            lower.append(p)
+        
+        upper = []
+        for p in reversed(points_sorted):
+            while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+                upper.pop()
+            upper.append(p)
+        convex_hull = lower[:-1] + upper[:-1]
+        
+        return convex_hull
+    
+    def hull_area(self, polygon_points):
+        polygon=Polygon(polygon_points)
+        return polygon.area
+    
+    def radial_distribution(self, dr):
+        hull_max=self.convex_hull_creation()
+        weights=[]
+        x=[]
+        y=[]
+        weight=0
+        #R= int(np.min(np.ptp(self.points, axis=0))/(2))
+        x_bounds = (np.abs(self.points[0, :].max())-np.abs(self.points[0, :].min()))/2
+        y_bounds = (np.abs(self.points[1, :].max())-np.abs(self.points[1, :].min()))/2
+        R=np.sqrt(x_bounds**2+y_bounds**2)
+        #x_min = np.min(np.abs(x0) - np.array(x_bounds))
+        #y_min = np.min(np.abs(y0) - np.array(y_bounds))
+        
+        #R = int(min(abs(x_min), abs(y_min)))
+        g= np.zeros(math.ceil(R/dr), dtype=float)
+        #g= np.zeros(dr, dtype=float)
+        n=self.calculate_mean_density()
+
+        for i, _ in enumerate(np.arange(0, R, dr)):
+            g_r =np.zeros((len(self.points)))
+            for j, point in enumerate(self.points):
+                r_i=(i+0.5)*dr
+                radius_min = r_i-dr/2
+                radius_max = r_i+dr/2
+                area=2*np.pi*r_i*dr
+                N_i=self.find_points(point[0], point[1], radius_min, radius_max)
+                #print(r_i, area)
+                # adding weights
+                intersection=self.intersection_area(point,radius_max, hull_max)-self.intersection_area(point,radius_min, hull_max)
+                weight= area/intersection
+                #g_r[j]=N_i/(area*n)
+                g_r[j]= N_i*weight/(area*n)
+                if np.abs(i-35)<=dr:
+                    weights.append(weight)
+            g[i]= np.mean(g_r)#N_i/(area*n)
+        return g, weights, self.points
